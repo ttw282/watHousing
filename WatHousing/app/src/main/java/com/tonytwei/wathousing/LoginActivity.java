2831c643935c287f2c6e756e6f0cb7fa70c6acf6
Tong Jia <tongjia19@gmail.com>
2015-06-12 20:44:21 -0400

    test commit

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
@@ -37,7 +37,7 @@ import java.util.List;
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
     /**
-     * A dummy authentication store containing known user names and passwords.
+     * A dummy authentication store containing known user names and passwords..
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{

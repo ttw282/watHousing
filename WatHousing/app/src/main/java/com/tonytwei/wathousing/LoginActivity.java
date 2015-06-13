16df1999ef0345526a5190215533b88edb70b32a
Tony Wei <t7wei@uwaterloo.ca>
2015-06-12 20:22:01 -0400

    redirect from login page

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentResolver;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -140,6 +141,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent i = new Intent(this, HousingListActivity.class);
+            startActivity(i);
         }
     }
 

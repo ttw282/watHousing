83a14c855493f4b08da84aea69368413fc12fe06
Michael Guo <mikeguo1992@gmail.com>
2015-06-12 20:45:00 -0400

    test commit

---
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/ListViewFragment.java
@@ -0,0 +1,23 @@
+package com.tonytwei.wathousing;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ListViewFragment extends Fragment {
+
+    public ListViewFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_view, container, false);
+    }
+}

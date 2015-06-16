5d722521d5da6c015067995997f6982de06aa109
Tony Wei <t7wei@uwaterloo.ca>
2015-06-16 00:12:21 -0400

    added reviews on housing detail page (TODO:modify URL)

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/HousingDetailActivity.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/HousingDetailActivity.java
@@ -1,12 +1,26 @@
 package com.tonytwei.wathousing;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 /**
@@ -52,6 +66,8 @@ public class HousingDetailActivity extends ActionBarActivity {
 
         ImageView img = (ImageView)findViewById(R.id.imageView);
         img.setImageResource(R.drawable.apartment);
+
+        new HttpAsyncTask().execute("http://mdguo.com/api/api.php");
     }
 
     @Override
@@ -74,5 +90,58 @@ public class HousingDetailActivity extends ActionBarActivity {
     public void WriteReview(View view) {
         Intent i = new Intent(this, WriteReview.class);
         startActivity(i);
+    }
+
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            ((TextView)findViewById(R.id.reviews)).setText(result);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
     }
 }

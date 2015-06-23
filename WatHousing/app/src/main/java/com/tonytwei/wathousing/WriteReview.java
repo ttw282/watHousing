dbb0e7295886c69aba9bfecc8e0980dccb7022ba
Tony Wei <t7wei@uwaterloo.ca>
2015-06-23 17:42:12 -0400

    remove some commented code

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/WriteReview.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/WriteReview.java
@@ -67,62 +67,6 @@ public class WriteReview extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-   /* public static String GET(String url){
-        InputStream inputStream = null;
-        String result = "";
-        try {
-
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
-
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
-
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        return result;
-    }
-
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-            return GET(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            ((TextView)findViewById(R.id.test)).setText(result);
-        }
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-
-    }*/
-
-
-
     public void postData(String valueIWantToSend) {
         HttpClient httpClient = new DefaultHttpClient();
         // replace with your url

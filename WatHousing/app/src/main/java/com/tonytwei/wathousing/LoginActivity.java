90e7267ad25043f82938f73a36182ee16a3903dd
thorntonsharks <junsongyu@gmail.com>
2015-07-24 15:22:27 -0400

    Revert "method"
    This reverts commit 1f79d5eb5d56d8b6daa6df3232710c0833982fb7.

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/LoginActivity.java
@@ -83,14 +83,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             }
         });
 
-        Button mEmailSignUpButton = (Button) findViewById(R.id.email_sign_up_button);
-        mEmailSignUpButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptSignUp();
-            }
-        });
-
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
@@ -105,10 +97,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    public void attemptSignUp(){
-        
-    }
-
     public void attemptLogin() {
         if (mAuthTask != null) {
             return;

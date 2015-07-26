07759c0b529da2aa07b6b435310633f3269a92ad
Tony Wei <t7wei@uwaterloo.ca>
2015-07-26 19:56:35 -0400

    remove get review notification saying updated

--- WatHousing/app/src/main/java/com/tonytwei/wathousing/HousingDetailActivity.java
+++ WatHousing/app/src/main/java/com/tonytwei/wathousing/HousingDetailActivity.java
@@ -138,7 +138,6 @@ public class HousingDetailActivity extends ActionBarActivity {
         // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Updated!", Toast.LENGTH_LONG).show();
             //((TextView)findViewById(R.id.reviews)).setText(result);
 
             try {
